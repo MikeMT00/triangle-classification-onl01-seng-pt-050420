4b82380433f1d2687e13fbae3bb5b48f23350b8b
Michael Thomas <michaelthomas0611@gmail.com>
2020-06-08 02:25:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,10 @@
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_accessor :equilateral, :isosceles, :scalene 
 
   class TriangleError < StandardError
-
+    def message
+      "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side."
+    end
   end
 
   def initialize(first_side, second_side, third_side)
@@ -11,8 +13,21 @@ class Triangle
     @third_side = third_side
   end
 
+  def violates_triangle_equality
+    if @first_side <= 0 || @second_side <= 0 || @third_side <= 0
+      raise TriangleError
+    elsif @first_side + @second_side <= @third_side || @first_side + @third_side <= @second_side || @second_side + @third_side <= @first_side
+      raise TriangleError
+    end
+  end
 
   def kind
-    
-  end
+    violates_triangle_equality
+    if @first_side == @second_side && @first_side == @third_side
+      :equilateral
+    elsif @first_side == @second_side || @second_side == @third_side || @first_side == @third_side
+      :isosceles
+    else
+      :scalene
+    end
 end
