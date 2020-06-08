cb19ba75e56b65138c5d4ad25b41c116cd4b0c46
Michael Thomas <michaelthomas0611@gmail.com>
2020-06-08 01:56:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,4 +6,14 @@ class Triangle
     @second_side = second_side
     @third_side = third_side
   end
+
+  def kind
+    violates_triangle_equality
+    if @first_side == @second_side && @first_side == @third_side
+      puts equilateral
+    elsif @first_side == @second_side || @second_side == @third_side || @first_side == @third_side
+      puts isosceles
+    else
+      puts scalene
+    end
 end
